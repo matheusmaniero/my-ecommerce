c206b57016787948fec0984acb89ea70a6977e38
Matheus <matheusmaniero@gmail.com>
2023-02-06 07:22:09 -0300

    Initial Commit: Project Dependencies

---
+++ src/main/java/com/myecommerce/myecommercesite/MyEcommerceSiteApplication.java
@@ -0,0 +1,13 @@
+package com.myecommerce.myecommercesite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyEcommerceSiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyEcommerceSiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/myecommerce/myecommercesite/MyEcommerceSiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myecommerce.myecommercesite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyEcommerceSiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
